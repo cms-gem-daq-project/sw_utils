e12259406adb9ca33b25a29a10e7c9dcadb170d9
Brian Dorney <brian.l.dorney@cern.ch>
2019-04-18 14:25:47 +0200

    user can now provide geographic address

--- scripts/doQC7.sh
+++ scripts/doQC7.sh
@@ -2,21 +2,29 @@
 
 helpstring="Usage: doQC7.sh -c [DetName]
     Options:
+        -a AMC slot number inside uTCA shelf
         -c chamber name, without '/' characters, e.g. 'GE11-X-S-CERN-0007'
+        -d Debugging flag, prints additional information
         -f starting step to begin testConnectivity.py with, defaults to 1, options [1,5]
-        -d Debugging flag, prints additional information"
+        -s uTCA shelf number"
 
+AMCSLOT="5"
 CHAMBER_NAME=""
+SHELF="2"
 STARTINGSTEP="1"
 
 OPTIND=1
-while getopts "c:f:h" opts
+while getopts "a:c:f:s:hd" opts
 do
     case $opts in
+        a)
+            AMCSLOT="$OPTARG";;
         c)
             CHAMBER_NAME="$OPTARG";;
         f)
             STARTINGSTEP="$OPTARG";;
+        s)
+            SHELF="$OPTARG";;
         h)
             echo >&2 "${helpstring}"
             kill -INT $$;;
@@ -71,8 +79,8 @@ echo "Terminal output will be saved too: ${LOGFILE}"
 echo "Calling Command: " 2>&1 | tee ${LOGFILE}
 
 # Call testConnectivity.py
-echo "testConnectivity.py -c ${CHAMBER_NAME} --checkCSCTrigLink -f${STARTINGSTEP} -n 200 -o 0x10 --shelf=2 -s5 --writePhases2File" 2>&1 | tee -a ${LOGFILE}
-testConnectivity.py -c ${CHAMBER_NAME} --checkCSCTrigLink -f${STARTINGSTEP} -n 200 -o 0x10 --shelf=2 -s5 --writePhases2File 2>&1 | tee -a ${LOGFILE}
+echo "testConnectivity.py -c ${CHAMBER_NAME} --checkCSCTrigLink -f${STARTINGSTEP} -n 50 -o 0x10 --shelf=${SHELF} -s${AMCSLOT} --writePhases2File" 2>&1 | tee -a ${LOGFILE}
+testConnectivity.py -c ${CHAMBER_NAME} --checkCSCTrigLink -f${STARTINGSTEP} -n 50 -o 0x10 --shelf=${SHELF} -s${AMCSLOT} --writePhases2File 2>&1 | tee -a ${LOGFILE}
 
 # Move the GBT phase scan log to ${DATA_PATH}/${CHAMBER_NAME}
 phaseScanLog=${ELOG_PATH}/gbtPhaseSettings.log
